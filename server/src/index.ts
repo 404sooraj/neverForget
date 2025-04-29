32d0cbfeb33b72cceada302f89f590354d13900e
sooraj <soorajnambiar8@gmail.com>
2025-04-30 00:11:58 +0530

    added db setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import { connectDB } from './config/database';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
 
 app.get('/', (_req, res) => {
   res.send('Hello, TypeScript + Express!');
