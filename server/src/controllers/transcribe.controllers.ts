418ac9419c8a5d1cb2c66d8e63aeda340d0e59a7
sooraj <soorajnambiar8@gmail.com>
2025-05-02 22:26:41 +0530

    changes

--- server/src/controllers/transcribe.controllers.ts
+++ server/src/controllers/transcribe.controllers.ts
@@ -14,27 +14,24 @@ export const transcribeAudio = (req: Request, res: Response): void => {
   const filePath = req.file.path;
   const outputTxt = `${filePath}.txt`;
 
-  exec(
-    `whisper ${filePath} --model base --output_format txt --output_dir uploads`,
-    (err) => {
+  exec(`whisper ${filePath} --model base  --output_dir uploads`, (err) => {
+    if (err) {
+      console.error("Whisper failed:", err);
+      res.status(500).json({ error: "Transcription failed" });
+      return;
+    }
+
+    fs.readFile(outputTxt, "utf8", (err, data) => {
       if (err) {
-        console.error("Whisper failed:", err);
-        res.status(500).json({ error: "Transcription failed" });
+        console.error("Reading transcript failed:", err);
+        res.status(500).json({ error: "Failed to read transcript" });
         return;
       }
 
-      fs.readFile(outputTxt, "utf8", (err, data) => {
-        if (err) {
-          console.error("Reading transcript failed:", err);
-          res.status(500).json({ error: "Failed to read transcript" });
-          return;
-        }
-
-        fs.unlinkSync(filePath);
-        fs.unlinkSync(outputTxt);
-        console.log("Transcription successful");
-        res.json({ transcript: data });
-      });
-    }
-  );
+      fs.unlinkSync(filePath);
+      fs.unlinkSync(outputTxt);
+      console.log("Transcription successful");
+      res.json({ transcript: data });
+    });
+  });
 };
